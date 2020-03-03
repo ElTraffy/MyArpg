3fc0b7db1f19eefa3416143bea6d2c2f59879ae4
ElTraffy <traffydeal@gmail.com>
2020-03-03 15:37:50 +0000

    camera + enemie
    enemie can already move into the player after seeing him and attack, fully animated

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mob : MonoBehaviour
+{
+    public float rangeAttack;
+    public float rangeSee;
+    public Transform player;
+
+    private NavMeshAgent mAgent;
+    private Animator mAnimator;
+    private bool mRunning = false;
+    private bool mAttack = false;
+    private bool seen = false;
+
+    void Start()
+    {
+        mAgent = GetComponent<NavMeshAgent>();
+        mAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRangeSee())
+        {
+            mRunning = true;
+            chase();
+        }
+
+        if (inRangeAttack())
+        {
+            mAgent.GetComponent<NavMeshAgent>().isStopped = true;
+            mRunning = false;
+            mAttack = true;
+        }
+        else
+        {
+            mAgent.GetComponent<NavMeshAgent>().isStopped = false;
+            mAttack = false;
+        }
+
+        mAnimator.SetBool("running", mRunning);
+        mAnimator.SetBool("canAttack", mAttack);
+    }
+
+    bool inRangeSee()
+    {
+        return Vector3.Distance(transform.position, player.position) < rangeSee;
+    }
+
+    bool inRangeAttack()
+    {
+        return Vector3.Distance(transform.position, player.position) < rangeAttack;
+    }
+
+    void chase()
+    {
+        mAgent.destination = player.position;
+    }
+}
