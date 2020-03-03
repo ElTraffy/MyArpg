70c4f596ff080354a87808adf06ac4be13873b9f
ElTraffy <traffydeal@gmail.com>
2020-03-03 13:18:37 +0000

    new char with animations
    whe are alliance now bois, new char, idle + running animation

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; //allows us to use navigation mesh
+
+public class playerMovement : MonoBehaviour
+{
+    private Animator mAnimator;
+
+    private NavMeshAgent mAgent;
+
+    private bool mRunning = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mAnimator = GetComponent<Animator>();
+        mAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                mAgent.destination = hit.point;
+            }
+        }
+
+        if (mAgent.remainingDistance <= mAgent.stoppingDistance)
+        {
+            mRunning = false;
+        }
+        else mRunning = true;
+
+        mAnimator.SetBool("running", mRunning);
+    }
+}
